9b5f05e2daae48721c4b44a9658aae320af450ba
Anna Kuszal <anna.kuszal@o2.pl>
2020-05-11 00:01:46 +0200

    Install & import socket.io

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const socket = require('socket.io');
 
 //const uuidv4 = require('uuid/v4');
 //const db = require('./db');
@@ -30,7 +31,13 @@ app.use((req, res) => {
     res.status(404).send('404 not found...');
   })
   
-app.listen(process.env.PORT || 8000, () => {
+const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
 
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('New socket!');
+
+});
